94c74d4eb51d8083452c9dfb0c069159d29ddcd9
Joseph Sarrao <josephsarrao@berkeley.edu>
2021-02-12 14:43:59 -0700

    submission

---
+++ Assets/Scripts/Enemy/EnemySpawnInfo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySpawnInfo : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("the name of this enemy")]
+    private string m_Name;
+    public string EnemyName
+    {
+        get
+        {
+            return m_Name;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("the prefab of this enemy")]
+    private GameObject m_EnemyGO;
+    public GameObject EnemyGO
+    {
+        get
+        {
+            return m_EnemyGO;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("the numer of seconds before the next enemy is spawned")]
+    private float m_TimeToNextSpawn;
+    public float TimeToNextSpawn
+    {
+        get
+        {
+            return m_TimeToNextSpawn;
+        }
+    }
+
+    [SerializeField]
+    [Tooltip("the number of enemies to spawn")]
+    private int m_NumberToSpawn;
+    public int NumberToSpawn
+    {
+        get
+        {
+            return m_NumberToSpawn;
+        }
+    }
+    #endregion
+}
